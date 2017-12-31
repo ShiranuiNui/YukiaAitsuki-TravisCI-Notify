85e9a3afbff4a55853121019a145d106f045b2e7
ShiranuiNui <shiranuisayaka@gmail.com>
2017-12-31 18:51:16 +0900

    [Add] Added Templates

---
+++ YukiaAitsuki-TravisCI-Notify.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace YukiaAitsuki_TravisCI_Notify.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
